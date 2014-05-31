8d2644031e5940a2eb3777fbc72af84dc4a7a0e4
Tad Heigel <theig072@gmail.com>
2014-05-31 00:33:51 -0400

    moved the footer and set the css margins

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,43 @@
-console.log('\'Allo \'Allo!');
+use strict';
+
+var tinyServer;
+var user = 'Thaddeous';
+var msgbox = _.template($('.chat-message').text());
+
+
+function renderChat(data) {
+  $('.textappend').empty();
+  for (var i = 0; i < data.length - 1; i += 1) {
+    if (data[i].message) {
+      var rendered = msgbox(data[i]);
+      $('.textappend').append(rendered);
+    }
+  }
+}
+
+function loadChat () {
+  $.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data) {
+    tinyServer = data;
+    renderChat(data);
+  });
+}
+
+function Message(user, message, time) {
+  this.user = user || '';
+  this.message = message || '';
+  this.time = time || '';
+}
+
+function refreshChat(info) {
+  $.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', info);
+}
+
+$('.send').click(function() {
+  var message = $('.message').val();
+  $('.message').val('');
+  var time = Date.now();
+  var outgoing = new Message(user, message, time);
+  refreshChat(outgoing);
+});
+
+setInterval(loadChat, 1000);
