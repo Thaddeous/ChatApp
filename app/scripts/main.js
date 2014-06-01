331be70e57e23d16ffc4700ddf3ef99e95626822
Tad Heigel <theig072@gmail.com>
2014-05-31 23:46:32 -0400

    readied for js test writing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,48 +1,4 @@
-'use strict';
-
-var tinyServer;
-var user = 'Snake-Like Dinosaur';
-var msgbox = _.template($('.chat-message').text());
-
-function renderChat(data) {
-	$('.textappend').empty();
-	for (var i = 0; i < data.length - 1; i += 1) {
-		if (data[i].message) {
-			var rendered = msgbox(data[i]);
-			$('.textappend').append(rendered);    
-		}  
-	}
-}
-
-function loadChat() {
-	$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data) {
-		tinyServer = data;
-		renderChat(data);  
-	});
-}
-
-function Message(user, message, time) {  
-	this.user = user || '';  
-	this.message = message || '';  
-	this.time = time || '';
-}
-
-function refreshChat(info) {  
-	$.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', info);
-}
-
-$('.send').click(function() {
-	var message = $('.message').val();  
-	$('.message').val('');
-	var time = Date.now();
-	var outgoing = new Message(user, message, time);
-	refreshChat(outgoing);
-});
-
-setInterval(loadChat, 1000);
-
-
-
+//'use strict';
 // // var gives a name to the below function where we are
 // // creating a template to be reusable 
 // var showMessages = _.template ($('.showMessages').text());
@@ -74,3 +30,56 @@ setInterval(loadChat, 1000);
 // $.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data){
 // 	chatInfo(data);
 // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var tinyServer;
+// var user = 'Snake-Like Dinosaur';
+// var msgbox = _.template($('.chat-message').text());
+
+// function renderChat(data) {
+// 	$('.textappend').empty();
+// 	for (var i = 0; i < data.length - 1; i += 1) {
+// 		if (data[i].message) {
+// 			var rendered = msgbox(data[i]);
+// 			$('.textappend').append(rendered);    
+// 		}  
+// 	}
+// }
+
+// function loadChat() {
+// 	$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data) {
+// 		tinyServer = data;
+// 		renderChat(data);  
+// 	});
+// }
+
+// function Message(user, message, time) {  
+// 	this.user = user || '';  
+// 	this.message = message || '';  
+// 	this.time = time || '';
+// }
+
+// function refreshChat(info) {  
+// 	$.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', info);
+// }
+
+// $('.send').click(function() {
+// 	var message = $('.message').val();  
+// 	$('.message').val('');
+// 	var time = Date.now();
+// 	var outgoing = new Message(user, message, time);
+// 	refreshChat(outgoing);
+// });
+
+// setInterval(loadChat, 1000);
