bf9939765f75aac2c9a831369ea9b6b7dbb04c19
Tad Heigel <theig072@gmail.com>
2014-06-02 01:03:37 -0400

    getJSON is now working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,97 @@
-//'use strict';
+'use strict';
+
+function username(){
+	$(".chat-message").html("<span class = 'bug'>TweatlBug: </span>Hello, what is your name?");
+}
+
+$(function(){
+	// username()
+    $("#textbox").keypress(function(event){
+        if(event.which == 13){
+        if ($("#enter").prop("checked")){
+			$("#send").click();
+			event.preventDefault();
+        }
+	}
+});
+
+    $("#send").click(function(){
+		var username = "<span class ='username' = >You: </span>"
+    	var newMessage = $("#textbox").val();
+    	$("#textbox").val("");
+    	var prevState = $(".chat-message").html();
+    		if (prevState.length > 3){
+    			prevState = prevState + "<br>";
+    		}
+    	$(".chat-message").html(prevState + username + newMessage);
+    	$(".chat-message").scrollTop($(".chat-message").prop("scrollHeight"));
+    })
+});
+
+var getData = _.template($(".messagedata").text());
+function renderData(data){
+	data.forEach(function(alldata){
+		var renderedData = getData(alldata);
+		$(".messages-box").append(renderedData);
+		console.log(renderedData)
+	});
+}
+
+$.getJSON("http://tiny-pizza-server.herokuapp.com/collections/chat-messages").done(function(alldata){
+	renderData(alldata);
+});
+
+
+// var tinyServer;
+// var user = 'meh';
+// var msgbox = _.template($('.chat-message-box').text());
+
+// function renderChat(data) {
+// 	$('.bottom-right-bar-box').empty();
+// 	for (var i = 0; i < data.length - 1; i += 1) {
+// 		if (data[i].message) {
+// 			var rendered = msgbox(data[i]);
+// 			$('.bottom-right-bar-box').append(rendered);    
+// 		}  
+// 	}
+// }
+
+// function loadChat() {
+// 	$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data) {
+// 		tinyServer = data;
+// 		renderChat(data);  
+// 	});
+// }
+
+// function Message(user, message, time) {  
+// 	this.user = user || '';  
+// 	this.message = message || '';  
+// 	this.time = time || '';
+// }
+
+// function refreshChat(info) {  
+// 	$.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', info);
+// }
+
+// $('.send').click(function() {
+// 	var message = $('.message').val();  
+// 	$('.message').val('');
+// 	var time = Date.now();
+// 	var outgoing = new Message(user, message, time);
+// 	refreshChat(outgoing);
+// });
+
+// setInterval(loadChat, 1);
+
+
+
+
+
+
+
+
+
+
 // // var gives a name to the below function where we are
 // // creating a template to be reusable 
 // var showMessages = _.template ($('.showMessages').text());
@@ -30,56 +123,3 @@
 // $.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data){
 // 	chatInfo(data);
 // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var tinyServer;
-// var user = 'Snake-Like Dinosaur';
-// var msgbox = _.template($('.chat-message').text());
-
-// function renderChat(data) {
-// 	$('.textappend').empty();
-// 	for (var i = 0; i < data.length - 1; i += 1) {
-// 		if (data[i].message) {
-// 			var rendered = msgbox(data[i]);
-// 			$('.textappend').append(rendered);    
-// 		}  
-// 	}
-// }
-
-// function loadChat() {
-// 	$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data) {
-// 		tinyServer = data;
-// 		renderChat(data);  
-// 	});
-// }
-
-// function Message(user, message, time) {  
-// 	this.user = user || '';  
-// 	this.message = message || '';  
-// 	this.time = time || '';
-// }
-
-// function refreshChat(info) {  
-// 	$.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', info);
-// }
-
-// $('.send').click(function() {
-// 	var message = $('.message').val();  
-// 	$('.message').val('');
-// 	var time = Date.now();
-// 	var outgoing = new Message(user, message, time);
-// 	refreshChat(outgoing);
-// });
-
-// setInterval(loadChat, 1000);
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,14 +1,11 @@
 /* global describe, it */
+// 'use strict';
 
-(function () {
-    'use strict';
-
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-              var answer = 2;
-              expect(2).to.equal(answer);
-            });
-        });
-    });
-})();
+// (function(){
+// 	describe("the input function", function(){
+// 		it("should accept text as input and print it to the console", function(){
+// 			var answer = 2;
+//             expect(2).to.equal(answer);
+// 		});
+// 	});
+})
